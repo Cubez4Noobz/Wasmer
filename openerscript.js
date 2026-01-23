c0a3cd769dfbc1069ff32c486d0fbe6b04ff26f8
ZacheryWallace <cubez4noobs50k@gmail.com>
2026-01-22 19:15:18 -0500

    fix stuff

--- openerscript.js
+++ openerscript.js
@@ -2,7 +2,7 @@ var username = prompt("Enter the username:");
 if (username == "isawnothing") {
     var password = prompt("Enter the password:");
     if (password == "1234567890987654321") {
-        window.location.href = "home.html";
+        window.location.replace("home.html");
     }
 }
 else {
